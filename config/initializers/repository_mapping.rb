1d7ad8e6f3eadacf8637c66b441c18638b43d019
Jason Roelofs <jasongroelofs@gmail.com>
2012-08-20 23:00:41 -0400

    Fix database seed data

--- config/initializers/repository_mapping.rb
+++ config/initializers/repository_mapping.rb
@@ -2,6 +2,6 @@ BlogRepository = InMemory::BlogRepo.new
 PostRepository = InMemory::PostRepo.new
 
 if Rails.env.development?
-  BlogRepository.save Blog.new("My Test Blog")
-  BlogRepository.save Blog.new("Rants and Raves")
+  BlogRepository.save Blog.new(:title => "My Test Blog")
+  BlogRepository.save Blog.new(:title => "Rants and Raves")
 end
