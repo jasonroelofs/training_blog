03502107445fa639b0a77a07d9e022dc46aaed77
Jason Roelofs <jasongroelofs@gmail.com>
2012-08-20 22:41:42 -0400

    Add Entity library from raidit
    Update load path to include lib/

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module TrainingBlog
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(
+      #{config.root}/lib
       #{config.root}/app/repositories
     )
 
