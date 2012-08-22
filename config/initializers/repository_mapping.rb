dc4255bde5bb4a828981763f441db09e37df047c
Jason Roelofs <jasongroelofs@gmail.com>
2012-08-21 21:04:53 -0400

    Only set test data here if using in-memory

--- config/initializers/repository_mapping.rb
+++ config/initializers/repository_mapping.rb
@@ -5,9 +5,10 @@ when "active_record"
 else
   BlogRepository = InMemory::BlogRepo.new
   PostRepository = InMemory::PostRepo.new
+
+  if Rails.env.development?
+    BlogRepository.save Blog.new(:title => "My Test Blog")
+    BlogRepository.save Blog.new(:title => "Rants and Raves")
+  end
 end
 
-if Rails.env.development?
-  BlogRepository.save Blog.new(:title => "My Test Blog")
-  BlogRepository.save Blog.new(:title => "Rants and Raves")
-end
